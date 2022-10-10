5157b489559d0c9e0f2232f9be9cdd91a608c9e5
Ken <fieldken@hotmail.com>
2022-10-10 16:06:20 +0200

    Add project files.

---
+++ DatabaseVoorAxel/Migrations/20221010135124_relation.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseVoorAxel.Migrations
+{
+    public partial class relation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GradeId",
+                table: "Personen",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personen_GradeId",
+                table: "Personen",
+                column: "GradeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Personen_Grades_GradeId",
+                table: "Personen",
+                column: "GradeId",
+                principalTable: "Grades",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Personen_Grades_GradeId",
+                table: "Personen");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Personen_GradeId",
+                table: "Personen");
+
+            migrationBuilder.DropColumn(
+                name: "GradeId",
+                table: "Personen");
+        }
+    }
+}
---
+++ DatabaseVoorAxel/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+/*Grade ib5 = new Grade()
+{
+    Name = "Informaticabeheer",
+    Year = 5
+};
+
+Persoon axel = new Persoon()
+{
+    Name = "Axel",
+    Age = 25,
+    Grade = ib5
+};
+
+using (ApplicationDbContext ctx = new ApplicationDbContext())
+{
+    ctx.Personen.Add(axel);
+    ctx.Personen.Add(new Persoon
+    {
+        Name = "Ken",
+        Age = 26,
+        Grade = ib5
+    });
+
+    ctx.SaveChanges();
+}*/
+
+using (ApplicationDbContext ctx = new ApplicationDbContext())
+{
+    foreach (Persoon student in ctx.Personen.Include(s => s.Grade))
+    {
+        Console.WriteLine($"{student.Name} - {student.Age} - {student.Grade.Year} {student.Grade.Name}");
+    }
+}
+
+public class ApplicationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder barbecue)
+    {
+        barbecue.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=AxelDatabase;Trusted_Connection=True;");
+    }
+
+    public DbSet<Persoon> Personen { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+}
+
+public class Persoon
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Grade Grade { get; set; }
+}
+
+public class Grade
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public ICollection<Persoon> Personen { get; set; }
+}
